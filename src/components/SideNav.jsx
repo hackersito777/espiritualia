a606eb202a4f439e99b15ebf4957736cd74de878
hackersito777 <yefersonjmo777@gmail.com>
2024-01-04 10:49:07 -0500

    comit223

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "../components/SideNav.css";
 
 const SideNav = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const openNav = () => {
+    setIsNavOpen(true);
+  };
+
+  const closeNav = () => {
+    setIsNavOpen(false);
+  };
+
   return (
     <>
-      <div id="mySidenav" className="sidenav">
-        <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>
-          &times;
-        </a>
-        <a href="#">About</a>
-        <a href="#">Services</a>
-        <a href="#">Clients</a>
-        <a href="#">Contact</a>
-      </div>
+      {isNavOpen && (
+        <div id="mySidenav" className="sidenav">
+          <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>
+            &times;
+          </a>
+          <a href="#">About</a>
+          <a href="#">Services</a>
+          <a href="#">Clients</a>
+          <a href="#">Contact</a>
+        </div>
+      )}
 
-      <span onClick={openNav}>open</span>
-      
+      <span onClick={openNav}></span>
     </>
   );
 };
