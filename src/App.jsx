a2a0f3bf2cb9fd0a6f4aa1d91529d082b25214c3
hackersito777 <yefersonjmo777@gmail.com>
2024-01-04 10:41:12 -0500

    comit22

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import Navbar from './components/NavBar'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AcercaDe from './components/AcercaDe';
 import Contacto from './components/Contacto';
+import SideNav from './components/SideNav';
 
 function App() {
   
 
   return (
     <Router>
-    
+    <SideNav/>
     <Navbar/>
     <Switch>
     <Route path="/" exact component={Inicio} />
---
+++ src/components/SideNav.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../components/SideNav.css";
+
+const SideNav = () => {
+  return (
+    <>
+      <div id="mySidenav" className="sidenav">
+        <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>
+          &times;
+        </a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+      </div>
+
+      <span onClick={openNav}>open</span>
+      
+    </>
+  );
+};
+
+export default SideNav;
